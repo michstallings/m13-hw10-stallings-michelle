816bd7cecb8bc6f4a757e3c175b9e3a78ebe38e8
michstallings <contact@michellestallings.com>
2018-11-17 14:57:08 -0800

    Commented out script

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-var section = document.getElementsByTagName("section")[0];
+//var section = document.getElementsByTagName("section")[0];
 
-var button = document.getElementsByTagName("button")[0];
+//var button = document.getElementsByTagName("button")[0];
 
 
-function slideToggle() {
+//function slideToggle() {
  
-   section.classList.toggle("hide");
+   //section.classList.toggle("hide");
 
-};
+//};
 
-button.addEventListener('click', slideToggle);
+//button.addEventListener('click', slideToggle);
 
 
 
