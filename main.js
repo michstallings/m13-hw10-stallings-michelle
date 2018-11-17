2384f5b79b1ac344903886d248d0637c97656fb3
michstallings <contact@michellestallings.com>
2018-11-17 14:56:22 -0800

    Added function and event listener

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var section = document.getElementsByTagName("section")[0];
 var button = document.getElementsByTagName("button")[0];
 
 
+function slideToggle() {
+ 
+   section.classList.toggle("hide");
+
+};
+
+button.addEventListener('click', slideToggle);
+
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
